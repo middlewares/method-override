16f0ceab35bfee0e89881c5693c4ebb1bb19ab1b
oscarotero <oom@oscarotero.com>
2016-12-24 10:41:42 +0100

    Updated http-middleware interface to 0.4 and php-cs-fixer to 2.0

--- src/MethodOverride.php
+++ src/MethodOverride.php
@@ -4,10 +4,10 @@ namespace Middlewares;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Interop\Http\Middleware\ServerMiddlewareInterface;
-use Interop\Http\Middleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
 
-class MethodOverride implements ServerMiddlewareInterface
+class MethodOverride implements MiddlewareInterface
 {
     const HEADER = 'X-Http-Method-Override';
 
